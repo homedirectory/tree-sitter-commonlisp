906d5ded2f5d32194eedc6bdadded5da91ed96d1
homedirectory <47361170+homedirectory@users.noreply.github.com>
2023-07-15 12:41:29 +0300

    define precedence of numbers and symbols

--- grammar.js
+++ grammar.js
@@ -1,4 +1,15 @@
 
+// 2.3.4 Symbols as Tokens
+// Any token that is not a potential number, does not contain a package marker,
+// and does not consist entirely of dots will always be interpreted as a
+// symbol.
+// Therefore, numbers should have higher precedence than symbols.
+
+const PREC = {
+    number: 50,
+    symbol: 40
+};
+
 const WHITESPACE = /[ \t\n\v\f\r\u{0085}\u{00A0}\u{1680}\u{2000}-\u{200A}\u{2028}\u{2029}\u{202F}\u{205F}\u{3000}]+/u;
 
 // 2.1.4 Character Syntax Types
