2f0f8e6cb9e981f17c9efd02bee500b2115ac3a1
homedirectory <47361170+homedirectory@users.noreply.github.com>
2023-07-15 12:41:08 +0300

    define character syntax types

--- grammar.js
+++ grammar.js
@@ -1,4 +1,20 @@
 
+// 2.1.4 Character Syntax Types
+
+const SYNTAX_TYPES = {
+
+    constituent: choice(
+        /[0-9:<=>?!@a-zA-Z$%&^_*{+}~]/, "\x08", ".", "\x7f", "/", "-", "[", "]"),
+
+    macro_char_term: /[;,"'`()]/,
+    macro_char_noterm: "#",
+
+    escape_single: "\\",
+    escape_multi: "|",
+
+};
+
+
 module.exports = grammar({
 
     name: 'commonlisp',
