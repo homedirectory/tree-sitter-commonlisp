a21cd68aa6c0750b6de209e28c76e3bda009c370
homedirectory <47361170+homedirectory@users.noreply.github.com>
2023-07-22 19:01:09 +0300

    add an example LISP file

---
+++ examples/hi.lisp
@@ -0,0 +1,45 @@
+(defun greet (a b c)
+  (declare (number x))
+  "greets you"
+  t ; true
+  nil #| 123 #| 321 |# |#
+  (print "hello world"))
+
+(defun fun (a &rest args &optional (x 1) &key (k1 3)))
+(defun fun (a &optional (x 1) &rest args &key (k1 3)))
+(defun fun (a &optional (x 1) &rest args &key (k1 3) &allow-other-keys))
+(defun fun (a &aux x (b 3)))
+
+(funcall #'+ 1 2)
+
+(setf vec #(1 2 3))
+(setf vec #5(1 2 3))
+
+#:abc
+
+#.123
+
+(setf path #p"/home/123")
+
+(setf arr1 #1A(1 2 3))
+
+'(1 . 2)
+
+`(1 ,2 ,@(3 4) ,.(3 4))
+
+(+ 123 #x123 #b111 #o777)
+
+pkg:sym
+:keyword
+
+((lambda (x y) (+ x y)) 1 2)
+
+(let ((x 5) (y 3)) (+ x y))
+(let* ((x 5) (y 3)) (+ x y))
+
+(defvar x 10 "hello")
+(defparameter *x* 100 "world")
+
+(setf ** 'huh)
+
+'(a b c)
